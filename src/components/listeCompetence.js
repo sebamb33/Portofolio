e49ae3015f721dbe6089b43e0354125020059939
sebAmb <sebastien.ambona33@gmail.com>
2021-08-15 08:56:07 +0200

    pc Portable modif

--- src/components/listeCompetence.js
+++ src/components/listeCompetence.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 const listeCompetence = () => {
-    return ( <div className="listeCompetences"></div>);
+    return ( 
+        <script src="https://kit.fontawesome.com/cbfce3ba07.js" crossorigin="anonymous"/>
+        <div className="listeCompetences">
+            <div className="uneCompetences">
+                <i class="fab fa-github"></i>
+                <p> GitHub</p>
+            </div>
+            <div className="uneCompetences">
+                <i class="fab fa-html5"></i>
+                <p> Html </p>
+            </div>
+            <div className="uneCompetences">
+                <i class="fab fa-css3-alt"></i>
+                <p> Css</p>
+            </div>
+            <div className="uneCompetences">
+            
+            </div>                
+        </div>
+        );
 }
  
 export default listeCompetence;
