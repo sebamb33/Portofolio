9cfeece8043b5bfaad526f7206174c6527410a1d
sebastien ambona <sebastien.ambona33@gmail.com>
2021-08-15 08:11:49 +0200

    Ajout de la gestion des compétences

---
+++ src/components/listeCompetence.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const listeCompetence = () => {
+    return ( <div className="listeCompetences"></div>);
+}
+ 
+export default listeCompetence;
---
+++ src/pages/Competences.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ElementPrincipal from "../components/ElementPrincipal";
+
+const Competences = () => {
+    return (
+    <div className="competences">
+         <ElementPrincipal/>
+    </div>  );
+}
+ 
+export default Competences;
